01e88cbfcfc77b1eb2517b71ee0838be2dfd69cf
kyunghoon <rlarudgns970@naver.com>
2022-07-29 13:31:20 +0900

    .

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -12,8 +12,15 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
-let taskList = []
+let tabs = document.querySelectorAll(".task-tabs div");
+let taskList = [];
+let mode ="all";
+let filterList=[];
 addButton.addEventListener("click",addTask);
+
+for(let i=1;i<tabs.length;i++){
+    tabs[i].addEventListener("click", function(event){filter(event)})
+}
 
 function addTask(){
     let task = {
@@ -27,22 +34,28 @@ function addTask(){
 }
 
 function render(){
+    let list = [];
+    if(mode == "all"){
+        list = taskList;
+    }else if(mode == "ongoing" || mode == "done"){
+        list = filterList;
+    }
     let resultHTML = '';
-    for(let i=0;i<taskList.length;i++){
-        if(taskList[i].isComplete == true){
+    for(let i=0; i<list.length; i++){
+        if(list[i].isComplete == true){
             resultHTML+=`<div class="task">
-            <div class="task-done">${taskList[i].taskContent}</div>
+            <div class="task-done">${list[i].taskContent}</div>
             <div>
-                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                <button onclick="deleteTask()">Delete</button>
+                <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                <button onclick="deleteTask('${list[i].id}')">Delete</button>
             </div>
         </div>`;
         }else{
             resultHTML += `<div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-            <button onclick="deleteTask()">Delete</button>
+            <button onclick="toggleComplete('${list[i].id}')">Check</button>
+            <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
     </div>`;
         }
@@ -62,9 +75,39 @@ function toggleComplete(id) {
     console.log(taskList);
 }
 
-function deleteTask(){
-    console.log("삭제하자");
+function deleteTask(id){
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1)
+            break;
+        }
+    }
+    render();
 }
+
+function filter(event){
+    mode = event.target.id;
+    filterList = [];
+    if(mode == "all"){
+        render();
+    }else if(mode == "ongoing"){
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }else if(mode == "done"){
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }
+}
+
+console.log(filterList);
 
 function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2, 9);
